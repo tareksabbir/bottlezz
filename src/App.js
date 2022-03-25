409d377b753a2f7c077ec11b6fe11f1cf08e574a
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2022-03-25 23:24:48 +0600

    header-update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './Components/Header/Header';
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-primary'> Site is running@!</h1>
+    <div>
+      <Header></Header>
+
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.png';
+import './Header.css'
+
+const Header = () => {
+    return (
+
+        <div className='head-bar'>
+            <nav className='navbar-part'>
+                <a id='logo' href=""><img src={logo} alt="" />Bottlezzz</a>
+                <div>
+                    <a href="">Shop</a>
+                    <a href="">Cart</a>
+                    <a href="">About</a>
+                </div>
+            </nav>
+
+        </div>
+
+
+    );
+};
+
+export default Header;
